67cb9b15c9a800305700626a1b8bb8ad939a9d2f
solomonbaez <solomon.james.baez@gmail.com>
2023-05-07 14:51:40 -0700

    Created telemetry module w/ associated functions

--- production_rust/src/main.rs
+++ production_rust/src/main.rs
@@ -1,25 +1,14 @@
 use production_rust::configuration::get_configuration;
 use production_rust::startup::run;
+use production_rust::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger");
+    let subscriber = get_subscriber("production_rust".into(), "info".into());
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("production_rust".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
--- production_rust/target/debug/deps/production_rust-57d02380d12e130a.d
+++ production_rust/target/debug/deps/production_rust-57d02380d12e130a.d
@@ -1,8 +1,8 @@
-/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-57d02380d12e130a.rmeta: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs
+/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-57d02380d12e130a.rmeta: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
 
-/Users/solomon/SBZ2P/production_rust/target/debug/deps/libproduction_rust-57d02380d12e130a.rlib: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs
+/Users/solomon/SBZ2P/production_rust/target/debug/deps/libproduction_rust-57d02380d12e130a.rlib: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
 
-/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-57d02380d12e130a.d: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs
+/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-57d02380d12e130a.d: src/lib.rs src/configuration.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
 
 src/lib.rs:
 src/configuration.rs:
@@ -10,3 +10,4 @@ src/routes/mod.rs:
 src/routes/health_check.rs:
 src/routes/subscriptions.rs:
 src/startup.rs:
+src/telemetry.rs:
