5f90a535051211de326f6417346498fda3c86534
solomonbaez <solomon.james.baez@gmail.com>
2023-05-10 11:36:36 -0700

    Integrated domain module w/ FormData validation

--- production_rust/src/routes/subscriptions.rs
+++ production_rust/src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -20,15 +20,17 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
     let name = match SubscriberName::parse(form.0.name) {
         Ok(name) => name,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
-    };
+    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&new_subscriber, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -47,7 +49,7 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        subscriber.email,
+        subscriber.email.as_ref(),
         subscriber.name.as_ref(),
         Utc::now()
     )
--- production_rust/target/debug/deps/production_rust-6f32f361383761d4.d
+++ production_rust/target/debug/deps/production_rust-6f32f361383761d4.d
@@ -1,13 +1,13 @@
-/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-6f32f361383761d4.rmeta: src/lib.rs src/configuration.rs src/domain/mod.rs src/domain/subscriber_name.rs src/domain/subscriber_email.rs src/domain/new_subscriber.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
+/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-6f32f361383761d4.rmeta: src/lib.rs src/configuration.rs src/domain/mod.rs src/domain/new_subscriber.rs src/domain/subscriber_email.rs src/domain/subscriber_name.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
 
-/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-6f32f361383761d4.d: src/lib.rs src/configuration.rs src/domain/mod.rs src/domain/subscriber_name.rs src/domain/subscriber_email.rs src/domain/new_subscriber.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
+/Users/solomon/SBZ2P/production_rust/target/debug/deps/production_rust-6f32f361383761d4.d: src/lib.rs src/configuration.rs src/domain/mod.rs src/domain/new_subscriber.rs src/domain/subscriber_email.rs src/domain/subscriber_name.rs src/routes/mod.rs src/routes/health_check.rs src/routes/subscriptions.rs src/startup.rs src/telemetry.rs
 
 src/lib.rs:
 src/configuration.rs:
 src/domain/mod.rs:
-src/domain/subscriber_name.rs:
-src/domain/subscriber_email.rs:
 src/domain/new_subscriber.rs:
+src/domain/subscriber_email.rs:
+src/domain/subscriber_name.rs:
 src/routes/mod.rs:
 src/routes/health_check.rs:
 src/routes/subscriptions.rs:
