ac2b45fadddf61c8c4a6ccc358734b2446e60b9f
solomonbaez <solomon.james.baez@gmail.com>
2023-05-09 23:53:53 -0700

    Updated tests to pinpoint API shortcomings

--- production_rust/tests/health_check.rs
+++ production_rust/tests/health_check.rs
@@ -111,6 +111,35 @@ async fn subscribe_returns_200() {
     assert_eq!(saved.name, "Aeonid Thiel")
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_with_empty_fields() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=raptor_imperialis%40@gmail.com", "empty name"),
+        ("name=Aeonid&email=", "empty email"),
+        ("name=Aeonid&email=invalid-email", "invalid email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return 200 OK with {}.",
+            error_message,
+        );
+    }
+}
+
 #[tokio::test] // Parametrized Test: missing form data returns 400
 async fn subscribe_returns_400() {
     let app = spawn_app().await; // Future
@@ -122,10 +151,11 @@ async fn subscribe_returns_400() {
         ("", "Missing both fields."),
     ];
 
-    for (_invalid_body, error_message) in test_cases {
+    for (invalid_body, error_message) in test_cases {
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
             .send()
             .await
             .expect("Failed to execute request.");
