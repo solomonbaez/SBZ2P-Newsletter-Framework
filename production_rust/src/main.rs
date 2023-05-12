4f3f659bfe57f12285a33d67ae10036e87774553
solomonbaez <solomon.james.baez@gmail.com>
2023-05-11 20:43:59 -0700

    Began modularizing startup logic

--- production_rust/src/main.rs
+++ production_rust/src/main.rs
@@ -1,9 +1,6 @@
 use production_rust::configuration::get_configuration;
-use production_rust::email_client::EmailClient;
-use production_rust::startup::run;
+use production_rust::startup::build;
 use production_rust::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -12,33 +9,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(1))
-        .connect_lazy_with(config.database.with_db());
 
-    let sender_email = config
-        .email_client
-        .sender()
-        .expect("Invalid email address.");
-
-    let timeout = config.email_client.timeout();
-    let email_client = EmailClient::new(
-        config.email_client.base_url,
-        sender_email,
-        config.email_client.auth_token,
-        timeout,
-    );
-
-    let address = format!("{}:{}", config.application.host, config.application.port);
-    let listener = TcpListener::bind(address)?;
-    println!(
-        "\n{}\n",
-        format_args!(
-            "Running Server -- http://127.0.0.1:{}",
-            listener.local_addr().unwrap().port()
-        )
-    );
-
-    run(listener, connection_pool, email_client)?.await?;
+    let server = build(config).await?;
+    server.await?;
     Ok(())
 }
--- production_rust/src/startup.rs
+++ production_rust/src/startup.rs
@@ -1,10 +1,43 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(config: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(1))
+        .connect_lazy_with(config.database.with_db());
+
+    let sender_email = config
+        .email_client
+        .sender()
+        .expect("Invalid email address.");
+
+    let timeout = config.email_client.timeout();
+    let email_client = EmailClient::new(
+        config.email_client.base_url,
+        sender_email,
+        config.email_client.auth_token,
+        timeout,
+    );
+
+    let address = format!("{}:{}", config.application.host, config.application.port);
+    let listener = TcpListener::bind(address)?;
+    println!(
+        "\n{}\n",
+        format_args!(
+            "Running Server -- http://127.0.0.1:{}",
+            listener.local_addr().unwrap().port()
+        )
+    );
+
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
