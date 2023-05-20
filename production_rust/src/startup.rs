0e235c6702b80d44361ae2f0518454b15bc93bd6
solomonbaez <solomon.james.baez@gmail.com>
2023-05-19 21:23:04 -0700

    Added redirection

--- production_rust/src/startup.rs
+++ production_rust/src/startup.rs
@@ -1,6 +1,8 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -85,6 +87,7 @@ fn run(
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/home", web::get().to(home))
             .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
