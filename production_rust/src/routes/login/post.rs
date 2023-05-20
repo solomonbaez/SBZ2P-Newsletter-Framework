67903f37ea60374e1e6515676e8f447ad374e857
solomonbaez <solomon.james.baez@gmail.com>
2023-05-20 09:02:03 -0700

    Added validation to the /login post request

--- production_rust/src/routes/login/post.rs
+++ production_rust/src/routes/login/post.rs
@@ -1,7 +1,9 @@
+use crate::authentication::{validate_credentials, Credentials};
 use actix_web::http::header::LOCATION;
 use actix_web::web;
 use actix_web::HttpResponse;
 use secrecy::Secret;
+use sqlx::PgPool;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -10,8 +12,22 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/home"))
-        .finish()
+pub async fn login(form: web::Form<FormData>, connection_pool: web::Data<PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    match validate_credentials(credentials, &connection_pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/home"))
+                .finish()
+        }
+        Err(_) => {
+            todo!()
+        }
+    }
 }
