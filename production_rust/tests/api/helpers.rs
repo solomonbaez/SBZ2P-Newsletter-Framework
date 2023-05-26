311a2438ead38daab36571a58d48dd70862005b1
solomonbaez <solomon.james.baez@gmail.com>
2023-05-25 21:53:04 -0700

    Added seed user for admin access

--- production_rust/tests/api/helpers.rs
+++ production_rust/tests/api/helpers.rs
@@ -127,7 +127,8 @@ impl TestUser {
         Self {
             user_id: Uuid::new_v4(),
             username: Uuid::new_v4().to_string(),
-            password: Uuid::new_v4().to_string(),
+            // password: Uuid::new_v4().to_string(),
+            password: "gloriainvigilata".into(),
         }
     }
 
@@ -141,6 +142,7 @@ impl TestUser {
         .hash_password(self.password.as_bytes(), &salt)
         .unwrap()
         .to_string();
+        dbg!(&password_hash);
 
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
