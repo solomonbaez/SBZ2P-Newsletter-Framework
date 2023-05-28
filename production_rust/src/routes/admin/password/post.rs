77c87805a136cd28aba49ba64d48ce502e8b09f8
solomonbaez <solomon.james.baez@gmail.com>
2023-05-28 09:09:00 -0700

    Finished admin logout and password change integration tests

--- production_rust/src/routes/admin/password/post.rs
+++ production_rust/src/routes/admin/password/post.rs
@@ -49,5 +49,9 @@ pub async fn change_password(
             AuthError::UnexpectedError(_) => Err(e500(e)),
         };
     }
-    todo!()
+    crate::authentication::change_password(user_id, form.0.new_password, &connection_pool)
+        .await
+        .map_err(e500)?;
+    FlashMessage::error("Your password has been changed.").send();
+    Ok(see_other("/admin/password"))
 }
