f5e90f7eb93a7ada0890cf5466ebfd17276aea12
solomonbaez <solomon.james.baez@gmail.com>
2023-06-01 22:34:15 -0700

    Began implementing idempotency concepts in the test suite -> one test will fail

--- production_rust/tests/api/newsletter.rs
+++ production_rust/tests/api/newsletter.rs
@@ -2,6 +2,41 @@ use crate::helpers::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestAp
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
+    let body = "name=Remus%20Ventanus&email=savior_of_calth%40gmail.com";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount_as_scoped(&app.email_server)
+        .await;
+    app.post_subscribers(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_link = create_unconfirmed_subscriber(app).await;
+    reqwest::get(confirmation_link.html_link)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+}
+
 #[tokio::test]
 async fn newsletters_unavailable_for_unconfirmed_subscribers() {
     let app = spawn_app().await;
@@ -75,37 +110,35 @@ async fn login_required_to_publish_newsletter() {
     assert_is_redirect_to(&response, "/login");
 }
 
-async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=Remus%20Ventanus&email=savior_of_calth%40gmail.com";
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
 
-    let _mock_guard = Mock::given(path("/email"))
+    Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
-        .named("Create unconfirmed subscriber")
         .expect(1)
-        .mount_as_scoped(&app.email_server)
+        .mount(&app.email_server)
         .await;
-    app.post_subscribers(body.into())
-        .await
-        .error_for_status()
-        .unwrap();
 
-    let email_request = &app
-        .email_server
-        .received_requests()
-        .await
-        .unwrap()
-        .pop()
-        .unwrap();
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string(),
+    });
 
-    app.get_confirmation_links(&email_request)
-}
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
 
-async fn create_confirmed_subscriber(app: &TestApp) {
-    let confirmation_link = create_unconfirmed_subscriber(app).await;
-    reqwest::get(confirmation_link.html_link)
-        .await
-        .unwrap()
-        .error_for_status()
-        .unwrap();
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published.</i></p>"));
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published.</i></p>"));
 }
