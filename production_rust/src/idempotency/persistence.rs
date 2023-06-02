074dff71edfda4593aa528f8698a75b00a36a4b3
solomonbaez <solomon.james.baez@gmail.com>
2023-06-02 16:18:35 -0700

    Added persistence submodule to idempotency

---
+++ production_rust/src/idempotency/persistence.rs
@@ -0,0 +1,36 @@
+use super::IdempotencyKey;
+use actix_web::HttpResponse;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[allow(dead_code)]
+#[derive(Debug, sqlx::Type)]
+#[sqlx(type_name = "header_pair")]
+struct HeaderPairRecord {
+    name: String,
+    value: Vec<u8>,
+}
+
+pub async fn get_saved_response(
+    connection_pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+) -> Result<Option<HttpResponse>, anyhow::Error> {
+    let _saved_response = sqlx::query!(
+        r#"
+        SELECT 
+            response_status_code,
+            response_headers as "response_headers: Vec<HeaderPairRecord>",
+            response_body
+        FROM idempotency
+        WHERE
+            user_id = $1 AND
+            idempotency_key = $2
+        "#,
+        user_id,
+        idempotency_key.as_ref()
+    )
+    .fetch_optional(connection_pool)
+    .await?;
+    todo!()
+}
--- production_rust/src/routes/admin/newsletters/get.rs
+++ production_rust/src/routes/admin/newsletters/get.rs
@@ -10,6 +10,7 @@ pub async fn new_newsletter_form(
     for m in flash_messages.iter() {
         writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
     }
+    let idempotency_key = uuid::Uuid::new_v4();
 
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
@@ -49,6 +50,7 @@ pub async fn new_newsletter_form(
             ></textarea>
         </label>
         <br>
+        <input hidden type="text" name="idempotency_key" value="{idempotency_key}">
         <button type="submit">Publish</button>
     </form>
     <p><a href="/admin/dashboard">&lt;- Back</a></p>
