95aa8841730d8104734a548c732e86df60523b86
solomonbaez <solomon.james.baez@gmail.com>
2023-06-08 22:54:15 -0700

    Integrated worker suite into the main body of the application

--- production_rust/src/issue_delivery_worker.rs
+++ production_rust/src/issue_delivery_worker.rs
@@ -1,11 +1,29 @@
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
+use crate::{configuration::Settings, startup::get_connection_pool};
 use sqlx::{PgPool, Postgres, Transaction};
 use std::time::Duration;
 use tracing::{field::display, Span};
 use uuid::Uuid;
 
-#[allow(dead_code)]
+pub async fn run_worker_until_stopped(configuration: Settings) -> Result<(), anyhow::Error> {
+    let connection_pool = get_connection_pool(&configuration.database);
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.auth_token,
+        timeout,
+    );
+    worker_loop(connection_pool, email_client).await
+}
+
 async fn worker_loop(
     connection_pool: PgPool,
     email_client: EmailClient,
