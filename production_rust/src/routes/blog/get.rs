f5fea57376129f9e19476e0ec2e696e1dd0e63c5
solomonbaez <solomon.james.baez@gmail.com>
2023-07-10 17:47:51 -0700

    Changed the general styling of the website -> /blog is the new template

--- production_rust/src/routes/blog/get.rs
+++ production_rust/src/routes/blog/get.rs
@@ -13,140 +13,145 @@ pub async fn blog(flash_messages: IncomingFlashMessages) -> Result<HttpResponse,
         .content_type(ContentType::html())
         .body(format!(
             r#"
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
-    <title>Blog</title>
-    <style>
-        body {{
-            /* background-color: #E4F9F5; */
-            margin: 0;
-            text-align: center;
-            font-family: "Merriweather", serif;
-        }}
-        
-        hr {{
-            border: dotted #EAF6F6 6px;
-            border-bottom: none;
-            width: 50%;
-            margin: 100px auto;
-        }}
-        
-        h1 {{
-            color: #66BFBF;
-            font-size: 5.625rem;
-            margin: 50px auto 0 auto;
-            font-family: "Sacramento", cursive;
-        }}
-    
-        h2 {{
-            color: #66BFBF;
-            font-size: 2.5rem;
-            font-family: "Montserrat", sans-serif;
-            font-weight: normal;
-        }}
-        
-        h3 {{
-            color: #11999E;
-            font-family: "Montserrat", sans-serif;
-        }}
-       
-        nav u1 {{
-            list-style: none;
-            display: flex;
-            justify-content: center;
-            margin-top: 20px;
-        }}
-        
-        nav u1 li {{
-            margin-right: 20px;
-        }}
-        
-        nav u1 li a {{
-            color: #777;
-            text-decoration: none;
-            transition: color 0.3s ease;
-        }}
-        
-        nav u1 li a:hover {{
-            color: #333;
-        }}
-
-        .blog-post {{
-            margin-bottom: 20px;
-            padding: 20px;
-            background: #f2f2f2;
-            border-radius: 5px;
-        }}
-        
-        .blog-post h3 {{
-            font-size: 24px;
-            margin-bottom: 10px;
-        }}
-        
-        .blog-post p {{
-            margin-bottom: 10px;
-        }}
-        
-        button[type="button"] {{
-            display: inline-block;
-            padding: 8px 12px;
-            justify-content: center;
-            padding: 10px 20px; 
-            border: 1px;
-            border-radius: 3px;
-            width: 125px;
-            cursor: pointer;
-            text-decoration: none;
-            background-color: #007bff;
-            color: #fff;
-            border-radius: 3px;
-            transition: background-color 0.3s ease;
-        }}
-
-        button:hover {{
-            background-color: #2A3F1B;
-        }}
-    </style>
-</head>
-<body>
-    <header>
-        <nav>
-            <u1>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/subscribe">Subscribe</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </u1>
-        </nav>
-    </header>
-        
-    <main>
-        <section>
-            <h2>Welcome to my Blog!</h2>
-            {msg_html}
-        </section>
-        <section>
-            <div class = "blog-post">
-                <h3>Blog Post Title</h3>
-                <p>blog content summary.</p>
-                <a href="/blog/post-slub"><button type="button">Read More</button></a>
-            </div>
-            <div class = "blog-post">
-                <h3>Blog Post Title</h3>
-                <p>blog content summary.</p>
-                <a href="/blog/post-slub"><button type="button">Read More</button></a>
-            </div>
-        </section>
-    </main>
-
-    <footer>
-        <p>&copy; 2023 Solomon Baez</p>
-        <p><a href="/login">admin login</a></p>
-    </footer>
-</body>
-</html>"#
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Blog</title>
+                <style>
+                    body {{
+                        margin: 0;
+                        text-align: center;
+                        font-family: "Merriweather", serif;
+                        background-color: #111;
+                        color: #fff;
+                    }}
+                    
+                    hr {{
+                        border: dotted #444 6px;
+                        border-bottom: none;
+                        width: 50%;
+                        margin: 100px auto;
+                    }}
+                    
+                    h1 {{
+                        color: #007bff;
+                        font-size: 5.625rem;
+                        margin: 50px auto 0 auto;
+                        font-family: "Sacramento", cursive;
+                    }}
+                    
+                    h2 {{
+                        color: #007bff;
+                        font-size: 2.5rem;
+                        font-family: "Montserrat", sans-serif;
+                        font-weight: normal;
+                    }}
+                    
+                    h3 {{
+                        color: #11999E;
+                        font-family: "Montserrat", sans-serif;
+                    }}
+                    
+                    nav ul {{
+                        list-style: none;
+                        display: flex;
+                        justify-content: center;
+                        margin-top: 20px;
+                    }}
+                    
+                    nav ul li {{
+                        margin-right: 20px;
+                    }}
+                    
+                    nav ul li a {{
+                        color: #fff;
+                        text-decoration: none;
+                        transition: color 0.3s ease;
+                    }}
+                    
+                    nav ul li a:hover {{
+                        color: #007bff;
+                    }}
+                    
+                    .blog-post {{
+                        margin-bottom: 20px;
+                        padding: 20px;
+                        background: #222;
+                        border-radius: 5px;
+                        display: inline-block;
+                        text-align: left;
+                    }}
+                    
+                    .blog-post h3 {{
+                        font-size: 24px;
+                        margin-bottom: 10px;
+                        color: #fff;
+                    }}
+                    
+                    .blog-post p {{
+                        margin-bottom: 10px;
+                    }}
+                    
+                    button[type="button"] {{
+                        display: inline-block;
+                        padding: 8px 12px;
+                        justify-content: center;
+                        padding: 10px 20px;
+                        border: 1px;
+                        border-radius: 3px;
+                        width: 125px;
+                        cursor: pointer;
+                        text-decoration: none;
+                        background-color: #007bff;
+                        color: #fff;
+                        border-radius: 3px;
+                        transition: background-color 0.3s ease;
+                    }}
+                    
+                    button:hover {{
+                        background-color: #003d5a;
+                    }}
+                </style>
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="/blog">Blog</a></li>
+                            <li><a href="/subscribe">Subscribe</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                    
+                <main>
+                    <section>
+                        <h2>Welcome to my Blog!</h2>
+                        {msg_html}
+                    </section>
+                    <section>
+                        <div class="blog-post">
+                            <h3>Blog Post Title</h3>
+                            <p>Blog content summary.</p>
+                            <a href="/blog/post-slub"><button type="button">Read More</button></a>
+                        </div>
+                        <div class="blog-post">
+                            <h3>Blog Post Title</h3>
+                            <p>Blog content summary.</p>
+                            <a href="/blog/post-slub"><button type="button">Read More</button></a>
+                        </div>
+                    </section>
+                </main>
+            
+                <footer>
+                    <p>&copy; 2023 Solomon Baez</p>
+                    <p><a href="/login">Admin Login</a></p>
+                </footer>
+            </body>
+            </html>
+            "#
         )))
 }
